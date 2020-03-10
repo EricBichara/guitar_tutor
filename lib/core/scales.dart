10b5987b04e2612c2d1eaa5b8295b1c2d701732d
Eric <eric.bichara@gmail.com>
2020-03-10 20:25:50 +0100

    scale fix

--- lib/core/scales.dart
+++ lib/core/scales.dart
@@ -1,7 +1,7 @@
 class Scales {
   static const List<int> ionian = [1, 3, 5, 6, 8, 10, 12];
   static const List<int> dorian = [1, 3, 4, 6, 8, 10, 11];
-  static const List<int> phrygian = [1, 3, 5, 6, 8, 10, 12];
+  static const List<int> phrygian = [1, 2, 4, 6, 8, 9, 11];
   static const List<int> lydian = [1, 3, 5, 7, 8, 10, 12];
   static const List<int> mixolydian = [1, 3, 5, 6, 8, 10, 11];
   static const List<int> aeolian = [1, 3, 4, 6, 8, 9, 11];
